7149231d6cb1474a4f83d6d347c6cd72f4ad8db8
Freyr666 <sky_rider_93@mail.ru>
2020-03-02 16:17:56 +0300

    iter

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 static void
 _on_destroy (gpointer data)
 {
-  g_print ("Context %s is being destroyed\n", (gchar *)data);
+  g_print ("Object %s is being destroyed\n", (gchar *)data);
   g_free (data);
 }
 
@@ -13,60 +13,84 @@ main (int argc, char** argv)
 {
   gboolean use_analysis = FALSE;
   GstGLContext *context;
-  gchar *name, *el_name;
+  GstGLWindow *window;
+  gchar *name;
   GstElement *bin, *src, *conv, *scrut, *sink;
   
   gst_init (&argc, &argv);
 
-  if (argc < 2 || g_strcmp0(argv[1], "-a") == 0)
-    use_analysis = TRUE;
-
-  bin = gst_pipeline_new ("pipeline");
-  src = gst_element_factory_make ("gltestsrc", NULL);
-  conv = gst_element_factory_make ("glcolorconvert", NULL);
-  if (use_analysis)
-    scrut = gst_element_factory_make ("videoanalysis", NULL);
-  else
-    scrut = gst_element_factory_make ("identity", NULL);
-  sink = gst_element_factory_make ("fakesink", NULL);
-
-  gst_bin_add_many (GST_BIN(bin), src, conv, scrut, sink, NULL);
-
-  if (!gst_element_link_many (src, conv, scrut, sink, NULL))
-    {
-      g_print ("failed to link elements");
-      exit(-1);
-    }
-
-  el_name = gst_object_get_name(GST_OBJECT(conv));
-  g_object_set_data_full (G_OBJECT(conv),
-                          "on_destroy",
-                          el_name,
-                          _on_destroy);
-
-  gst_element_set_state (bin, GST_STATE_PLAYING);
-
-  sleep (5);
+  for (int iter = 0; iter < 2; iter++) {
   
-  g_print ("clock_attach_wait returned\n");
-  g_object_get (conv, "context", &context, NULL);
-  name = gst_object_get_name(GST_OBJECT(context));
-  g_print ("Found context %s\n", name);
-  g_object_set_data_full (G_OBJECT(context),
-                          "on_destroy",
-                          name,
-                          _on_destroy);
-  gst_object_unref (context);
+    if (argc < 2 || g_strcmp0(argv[1], "-a") == 0)
+      use_analysis = TRUE;
 
-  g_print ("Context refcount %d\n", GST_OBJECT_REFCOUNT_VALUE(context));
+    bin = gst_pipeline_new ("pipeline");
+    src = gst_element_factory_make ("videotestsrc", NULL);
+    conv = gst_element_factory_make ("glupload", NULL);
+    if (use_analysis)
+      scrut = gst_element_factory_make ("videoanalysis", NULL);
+    else
+      scrut = gst_element_factory_make ("identity", NULL);
+    sink = gst_element_factory_make ("fakesink", NULL);
 
-  sleep (5);
-  g_print ("clock_id_wait returned\n");
-  gst_element_set_state (bin, GST_STATE_NULL);
+    gst_bin_add_many (GST_BIN(bin), src, conv, scrut, sink, NULL);
 
-  gst_object_unref (G_OBJECT (bin));
-  g_print ("Context refcount %d\n", GST_OBJECT_REFCOUNT_VALUE(context));
+    if (!gst_element_link_many (src, conv, scrut, sink, NULL))
+      {
+        g_print ("failed to link elements");
+        exit(-1);
+      }
+
+    name = gst_object_get_name(GST_OBJECT(conv));
+    g_object_set_data_full (G_OBJECT(conv),
+                            "on_destroy",
+                            name,
+                            _on_destroy);
+
+    name = gst_object_get_name(GST_OBJECT(bin));
+    g_object_set_data_full (G_OBJECT(bin),
+                            "on_destroy",
+                            name,
+                            _on_destroy);
+
+    gst_element_set_state (bin, GST_STATE_PLAYING);
+
+    sleep (5);
   
-  sleep (5);
+    g_print ("clock_attach_wait returned\n");
+    g_object_get (conv, "context", &context, NULL);
+    name = gst_object_get_name(GST_OBJECT(context));
+    g_print ("Found context %s\n", name);
+    g_object_set_data_full (G_OBJECT(context),
+                            "on_destroy",
+                            name,
+                            _on_destroy);
+    window = gst_gl_context_get_window (context);
+    name = gst_object_get_name(GST_OBJECT(window));
+    g_print ("Got window %s\n", name);
+    g_object_set_data_full (G_OBJECT(window),
+                            "on_destroy",
+                            name,
+                            _on_destroy);
+
+    gst_object_unref (window);
+    gst_object_unref (context);
+
+    g_print ("Context refcount %d\n", GST_OBJECT_REFCOUNT_VALUE(context));
+
+    sleep (10);
+    g_print ("clock_id_wait returned\n");
+    gst_element_set_state (bin, GST_STATE_NULL);
+
+    gst_object_unref (G_OBJECT (bin));
+    g_print ("Context refcount %d\n", GST_OBJECT_REFCOUNT_VALUE(context));
+
+    // GstGLContext refcount equals 16 here no matter what
+    for (int i = 0; i < 16; i++)
+      gst_object_unref (context);
+  
+    sleep (5);
+
+  }
   
 }
